824d891f13427159f6f5bda9dfa9f228cc886a33
Simon Meyer <simon.meyer@mein.gmx>
2024-03-31 15:50:52 +0200

    Added options to set the Entity Attributes manually and to not spawn the Entity in creative worlds

--- src/main/java/com/aguga/HorseSpawn.java
+++ src/main/java/com/aguga/HorseSpawn.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.DolphinEntity;
 import net.minecraft.entity.passive.DonkeyEntity;
 import net.minecraft.entity.passive.HorseEntity;
@@ -51,10 +52,17 @@ public class HorseSpawn implements ModInitializer
 		{
 			HorseEntity horseEntity = EntityType.HORSE.create(serverWorld);
 
-			if (horseEntity == null || isNotNew)
+			if(horseEntity == null || isNotNew)
 				return;
 
-			horseEntity.initialize(serverWorld, serverWorld.getLocalDifficulty(player.getBlockPos()), SpawnReason.MOB_SUMMONED, null, null);
+			if(CONFIG.randomAttributes())
+				horseEntity.initialize(serverWorld, serverWorld.getLocalDifficulty(player.getBlockPos()), SpawnReason.MOB_SUMMONED, null, null);
+			else
+			{
+				horseEntity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue(blocksPerSecToSpeed(CONFIG.speed()));
+				horseEntity.getAttributeInstance(EntityAttributes.HORSE_JUMP_STRENGTH).setBaseValue(jumpHeightToJumpStrength(CONFIG.jump()));
+				horseEntity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(CONFIG.health());
+			}
 
 			horseEntity.setTame(true);
 			if(CONFIG.saddle())
@@ -72,7 +80,14 @@ public class HorseSpawn implements ModInitializer
 			if (donkeyEntity == null || isNotNew)
 				return;
 
-			donkeyEntity.initialize(serverWorld, serverWorld.getLocalDifficulty(player.getBlockPos()), SpawnReason.MOB_SUMMONED, null, null);
+			if(CONFIG.randomAttributes())
+				donkeyEntity.initialize(serverWorld, serverWorld.getLocalDifficulty(player.getBlockPos()), SpawnReason.MOB_SUMMONED, null, null);
+			else
+			{
+				donkeyEntity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue(blocksPerSecToSpeed(CONFIG.speed()));
+				donkeyEntity.getAttributeInstance(EntityAttributes.HORSE_JUMP_STRENGTH).setBaseValue(jumpHeightToJumpStrength(CONFIG.jump()));
+				donkeyEntity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(CONFIG.health());
+			}
 
 			donkeyEntity.setTame(true);
 			if(CONFIG.saddle())
@@ -95,7 +110,43 @@ public class HorseSpawn implements ModInitializer
 			dolphinEntity.setPosition(player.getX(), player.getY(), player.getZ());
 			serverWorld.spawnEntity(dolphinEntity);
 		}
+	}
 
+	public double blocksPerSecToSpeed(double blocksPerSec)
+	{
+		return blocksPerSec / 42.157796;
+	}
 
+	public double jumpStrengthToJumpHeight(double strength)
+	{
+		double height = 0;
+		while(strength > 0) {
+			height += strength;
+			strength = (strength - .08) * .98 * .98;
+		}
+		return height;
+	}
+
+	public double jumpHeightToJumpStrength(double height)
+	{
+		double guess = 0.35;
+		double deviation = height - jumpStrengthToJumpHeight(guess);
+
+		while(deviation > 0.005)
+		{
+			if(deviation > 10)
+				guess += 0.8;
+			else if(deviation > 5)
+				guess += 0.4;
+			else if(deviation > 1)
+				guess += 0.02;
+			else if(deviation > 0.2)
+				guess += 0.01;
+			else if(deviation > 0)
+				guess += 0.0005;
+			deviation = height - jumpStrengthToJumpHeight(guess);
+		}
+
+		return guess;
 	}
 }
--- src/main/java/com/aguga/config/ConfigModel.java
+++ src/main/java/com/aguga/config/ConfigModel.java
@@ -2,6 +2,8 @@ package com.aguga.config;
 
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
+import io.wispforest.owo.config.annotation.RangeConstraint;
+import io.wispforest.owo.config.annotation.SectionHeader;
 
 
 @SuppressWarnings("unused")
@@ -9,13 +11,22 @@ import io.wispforest.owo.config.annotation.Modmenu;
 @Config(name = "horse-spawn-config", wrapperName = "HorseSpawnConfig")
 public class ConfigModel
 {
+    @SectionHeader("General")
     public Choices Mob = Choices.HORSE;
     public enum Choices
     {
         HORSE, DONKEY, DOLPHIN
     }
-
-    public boolean spawnInCreative = true;
     public boolean saddle = true;
     public boolean chest = true;
+    public boolean spawnInCreative = true;
+
+    @SectionHeader("Attributes")
+    public boolean randomAttributes = true;
+    @RangeConstraint(min = 1, max = 40)
+    public int health = 24;
+    @RangeConstraint(min = 0.1, max = 24)
+    public double speed = 8;
+    @RangeConstraint(min = 0.1, max = 16.5)
+    public double jump = 3;
 }
